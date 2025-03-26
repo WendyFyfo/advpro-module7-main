03e2626f5e2cfb843eb5879d211a81f46e1ab71a
WendyFyfo <wendyfyfo@gmail.com>
2025-03-26 18:00:25 +0700

    [FIX] fix typo in some files

--- src/model/subscriber.rs
+++ src/model/subscriber.rs
@@ -2,8 +2,8 @@ use rocket::serde::{Deserialize, Serialize};
 use rocket::log;
 use rocket::serde::json::to_string;
 use rocket::tokio;
-use bambangshop::: REQWEST_CLIENT;
-use crate:::model:notification::Notification;
+use bambangshop::REQWEST_CLIENT;
+use crate::model::notification::Notification;
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
 #[serde(crate = "rocket::serde")]
--- src/repository/subscriber.rs
+++ src/repository/subscriber.rs
@@ -1,15 +1,15 @@
-use dashmap::Dashmap;
+use dashmap::DashMap;
 use lazy_static::lazy_static;
 use crate::model::subscriber::Subscriber;
 
 //Singleton of Database
 lazy_static! {
-    static ref SUBSCRIBERS: Dashmap<String, Dashmap<String, Subscriber>> = Dashmap::new();
+    static ref SUBSCRIBERS: DashMap<String, DashMap<String, Subscriber>> = DashMap::new();
 }
 
 pub struct SubscriberRepository;
 
-imp SubscriberRepository {
+impl SubscriberRepository {
     pub fn add(product_type: &str, subscriber: Subscriber) -> Subscriber {
         let subscriber_value = subscriber.clone();
         if SUBSCRIBERS.get(product_type).is_none() {

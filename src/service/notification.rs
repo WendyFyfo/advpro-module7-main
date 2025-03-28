b0493b1635ba9b5f1bdaa21a2e48ead00d4596ef
WendyFyfo <wendyfyfo@gmail.com>
2025-03-28 17:36:44 +0700

    [refactor] delete unused imports

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -1,11 +1,10 @@
-use std::ops::Sub;
 use std::thread;
 
 use bambangshop::{Result, compose_error_response};
 use rocket::http::Status;
 use crate::model::notification::Notification;
 use crate::model::product::Product;
-use crate::model::subscriber::{self, Subscriber};
+use crate::model::subscriber::Subscriber;
 use crate::repository::subscriber::SubscriberRepository;
 
 pub struct NotificationService;
